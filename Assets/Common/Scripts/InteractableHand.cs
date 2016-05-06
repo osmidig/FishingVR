e5ca6306bf18ee7954613ef1c900bdb0a1e38b07
David Norberg <david.norberg92@gmail.com>
2016-05-06 21:34:01 +0200

    VR interaction.

---
+++ Assets/Common/Scripts/InteractableHand.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableHand : MonoBehaviour
+{
+    [SerializeField] private GameObject m_trackedModel;
+
+    private InteractableItemBase m_heldObject;
+
+    private int m_deviceIndex = -1;
+    private SteamVR_Controller.Device m_device;
+
+    public void Detach()
+    {
+        m_heldObject = null;
+        m_trackedModel.SetActive(true);
+    }
+
+    void SetDeviceIndex(int index)
+    {
+        m_deviceIndex = index;
+        m_device = SteamVR_Controller.Input(index);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (m_device.GetPressDown(Valve.VR.EVRButtonId.k_EButton_Grip))
+        {
+            if (m_heldObject == null)
+            {
+                InteractableItemBase item = other.GetComponentInParent<InteractableItemBase>();
+                if (item != null)
+                {
+                    if (item.Attach(transform, m_deviceIndex))
+                    {
+                        m_heldObject = item;
+                        m_trackedModel.SetActive(false);
+                    }
+                }
+            }
+            else
+            {
+                m_heldObject.Detach();
+                m_heldObject = null;
+                m_trackedModel.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Common/Scripts/InteractableItemBase.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableItemBase : MonoBehaviour
+{
+
+    private bool m_attached = false;
+    private SteamVR_Controller.Device m_device;
+
+    private Transform m_origParent;
+    private Transform m_transform;
+
+    protected virtual void Awake()
+    {
+        m_transform = transform;
+    }
+
+    public bool Attach(Transform parent, int deviceIndex)
+    {
+        if (m_attached) return false;
+
+        m_attached = true;
+        m_device = SteamVR_Controller.Input(deviceIndex);
+
+        m_origParent = m_transform.parent;
+        m_transform.SetParent(parent, false);
+        m_transform.localPosition = Vector3.zero;
+        m_transform.localRotation = Quaternion.identity;
+
+        return true;
+    }
+
+    public void Detach()
+    {
+        m_attached = false;
+        m_device = null;
+        m_transform.SetParent(m_origParent, true);
+    }
+}
