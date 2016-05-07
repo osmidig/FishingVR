6bb24cb5e48f376a60bae5bcc48083b46205d509
David Norberg <david.norberg92@gmail.com>
2016-05-07 12:57:26 +0200

    Throw objects

--- Assets/Common/Scripts/InteractableItemBase.cs
+++ Assets/Common/Scripts/InteractableItemBase.cs
@@ -3,19 +3,46 @@ using System.Collections;
 
 public class InteractableItemBase : MonoBehaviour
 {
+    [SerializeField] private bool m_attachable = true;
 
     private bool m_attached = false;
     private SteamVR_Controller.Device m_device;
 
+    private Rigidbody m_rigidbody;
+
+    private Vector3 m_previousPosition;
+    private Quaternion m_previousRotation;
+    private Vector3 m_previousForward;
+
     private Transform m_origParent;
     private Transform m_transform;
+
+    public bool Attachable
+    {
+        get { return m_attachable; }
+    }
 
     protected virtual void Awake()
     {
         m_transform = transform;
+        m_rigidbody = GetComponent<Rigidbody>();
     }
 
-    public bool Attach(Transform parent, int deviceIndex)
+    protected virtual void Update()
+    {
+
+    }
+
+    protected virtual void LateUpdate()
+    {
+
+
+        m_previousPosition = m_transform.position;
+        m_previousRotation = m_transform.rotation;
+        m_previousForward = m_transform.forward;
+    }
+
+    public bool Attach(Transform parent, int deviceIndex, bool worldPositionStays)
     {
         if (m_attached) return false;
 
@@ -23,9 +50,15 @@ public class InteractableItemBase : MonoBehaviour
         m_device = SteamVR_Controller.Input(deviceIndex);
 
         m_origParent = m_transform.parent;
-        m_transform.SetParent(parent, false);
-        m_transform.localPosition = Vector3.zero;
-        m_transform.localRotation = Quaternion.identity;
+        m_transform.SetParent(parent, worldPositionStays);
+
+        if(!worldPositionStays)
+        {
+            m_transform.localPosition = Vector3.zero;
+            m_transform.localRotation = Quaternion.identity;
+        }
+
+        if (m_rigidbody != null) m_rigidbody.isKinematic = true;
 
         return true;
     }
@@ -35,5 +68,31 @@ public class InteractableItemBase : MonoBehaviour
         m_attached = false;
         m_device = null;
         m_transform.SetParent(m_origParent, true);
+
+        if (m_rigidbody != null)
+        {
+            m_rigidbody.isKinematic = false;
+
+            Vector3 deltaPos = m_transform.position - m_previousPosition;
+            Vector3 newVel = deltaPos / Time.deltaTime;
+
+            m_rigidbody.AddForce(newVel, ForceMode.VelocityChange);
+
+            /*
+            Quaternion deltaRot = Quaternion.Inverse(m_transform.rotation) * m_previousRotation;
+            Vector3 newTorque = deltaRot.eulerAngles / Time.deltaTime;
+
+            m_rigidbody.AddTorque(newTorque, ForceMode.VelocityChange);
+            */
+
+            Vector3 x = Vector3.Cross(m_previousForward, m_transform.forward);
+            float theta = Mathf.Asin(x.magnitude);
+            Vector3 w = x.normalized * theta / Time.fixedDeltaTime;
+
+            Quaternion q = transform.rotation * m_rigidbody.inertiaTensorRotation;
+            Vector3 newTorque = q * Vector3.Scale(m_rigidbody.inertiaTensor, (Quaternion.Inverse(q) * w));
+
+            m_rigidbody.AddTorque(newTorque, ForceMode.Impulse);
+        }
     }
 }
