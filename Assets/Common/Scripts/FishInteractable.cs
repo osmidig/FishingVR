aa85f388d39742fdab6883510c8ededb45577c3e
David Norberg <david.norberg92@gmail.com>
2016-05-07 23:28:58 +0200

    merge?

---
+++ Assets/Common/Scripts/FishInteractable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishInteractable : InteractableItemBase
+{
+    public float m_ScaleRange = 0.3f;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        float newScale = Random.Range( 1.0f - m_ScaleRange, 1.0f + m_ScaleRange );
+
+        m_minHookedTension = Mathf.Clamp( m_minHookedTension * newScale, 0.1f, 1.9f );
+        m_maxHookedTension = Mathf.Clamp( m_maxHookedTension * newScale, 0.1f, 1.9f );
+
+        transform.localScale = transform.localScale * newScale;
+    }
+}
